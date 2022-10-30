16ac376607f1c333bf449baacaf2c603df2c5a82
Deva_kumar.N <83574761+Devakumar96@users.noreply.github.com>
2022-10-30 09:03:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// fetch() method: its defined on the window object, which we can use to perform request.
+// This method will return promise
+// promise will either be fuilfilled or rejected
+
+fetch('https://fakestoreapi.com/products').then((data)=>{
+    // console.log(data);
+    return data.json();
+}).then((completedata)=>{
+    // console.log(completedata);
+
+    let data1="";
+    completedata.map((values)=>{
+        data1 +=`<div class="card">
+        <h1 class="title">${values.title}</h1>
+        <img src=${values.image} alt="img" class="images">
+        <p>${values.description}</p>
+        <p class="category">${values.category}</p>
+        <p class="price">${values.price}</p>
+      </div>`
+    });
+
+    document.getElementById("cards").innerHTML=data1
+
+}).catch((err)=>{
+    console.log(err);
+})
